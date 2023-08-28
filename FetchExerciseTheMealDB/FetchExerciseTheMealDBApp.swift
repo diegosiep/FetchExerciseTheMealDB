617b5cfc909f71575d820f78d89809545c843e5b
diegosiep <96800577+diegosiep@users.noreply.github.com>
2023-08-28 12:08:12 -0600

    Initial Commit

---
+++ FetchExerciseTheMealDB/FetchExerciseTheMealDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchExerciseTheMealDBApp.swift
+//  FetchExerciseTheMealDB
+//
+//  Created by Diego Sierra on 28/08/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchExerciseTheMealDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
